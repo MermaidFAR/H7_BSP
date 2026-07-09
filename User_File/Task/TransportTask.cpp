e052181a153bec8c41e9035fa4b7e5165cf541ad
FAR <3292656460@qq.com>
2026-07-09 17:06:15 +0800

    chore:一些杂项

--- User_File/Task/TransportTask.cpp
+++ User_File/Task/TransportTask.cpp
@@ -25,9 +25,13 @@
 extern "C" void Transport_Task(void *argument)
 {
     MX_USB_DEVICE_Init();
-    
+    EricTool_USB.Set_Data(3,
+                          (int) &Debug_IMU_Data.Euler_Yaw_rad,
+                          (int) &Debug_IMU_Data.Euler_Pitch_rad,
+                          (int) &Debug_IMU_Data.Euler_Roll_rad);
     for (;;)
     {
+        EricTool_USB.TIM_1ms_Write_PeriodElapsedCallback();
         osDelay(1);
     }
 }
