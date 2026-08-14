1e905f19516f542b80be2a01fe3618d7cb23676f
Echo <echo@marinaecho.space>
2026-08-14 22:02:29 +0800

    fix:方便调试用

--- User_File/Task/TransportTask.cpp
+++ User_File/Task/TransportTask.cpp
@@ -8,8 +8,10 @@
 
 /* Includes ------------------------------------------------------------------*/
 
-#include "user_task.h"
+#include "sys_debug.h"
 #include "usb_device.h"
+#include "user_task.h"
+
 
 /* Private macros ------------------------------------------------------------*/
 
@@ -24,7 +26,7 @@
 extern "C" void Transport_Task(void *argument)
 {
     MX_USB_DEVICE_Init();
-    // EricTool_USB.Set_Data(2, (int) &Balance_Instance.Target_Pos, (int) &Balance_Instance.Current_Pos);
+    EricTool_USB.Set_Data(3, (int) &Debug_IMU_Data.Euler_Yaw_rad, (int) &Debug_IMU_Data.Euler_Pitch_rad, (int) &Debug_IMU_Data.Euler_Roll_rad);
     for (;;)
     {
         EricTool_USB.TIM_1ms_Write_PeriodElapsedCallback();
